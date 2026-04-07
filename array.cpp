09b64b48feddae1d5c74c5893862061a5f01d905
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-07 17:47:01 +0700

    Mengubah elemen array

--- array.cpp
+++ array.cpp
@@ -5,4 +5,9 @@ int main()
 {
 int dataku[5] = {10,20,30,40,50};
 int datamu[5];
+
+cout << "Isi data kedua : " << dataku[1] << endl;
+dataku[1] = 200;
+cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
+
 }

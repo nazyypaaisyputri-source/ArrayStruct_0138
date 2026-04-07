7193fac7e045544a797c93faf7310688b212096e
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-07 18:43:14 +0700

    Menambahkan input nested struct pada data orang

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -13,3 +13,20 @@ struct Orang
     detailAlamat alamat;
     int umur;
 };
+int main()
+{
+        {
+        //Deklarasi variabel struct
+        Orang mhs;
+        //Mengisi data
+        cout << "Isikan data nama : ";
+        cin >> mhs.nama;
+        cout << "Isikan data desa : ";
+        cin >> mhs.alamat.desa;
+        cout << "Isikan data kota : ";
+        cin >> mhs.alamat.kota;
+        cout << "Isikan data umur : ";
+        cin >> mhs.umur;
+        cout << endl;
+        }
+}

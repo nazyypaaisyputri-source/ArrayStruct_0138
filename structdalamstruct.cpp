c03d90cb593ff730feeb9050b438200badb3f5ae
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-07 18:40:06 +0700

    Menambahkan struct detail alamat

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};

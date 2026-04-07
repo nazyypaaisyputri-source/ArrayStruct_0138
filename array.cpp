14afc9f2f70121b7f57fbebcfd358ed2bcc05c48
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-04-07 17:52:12 +0700

    Input dan Output data array secara manual

--- array.cpp
+++ array.cpp
@@ -6,8 +6,15 @@ int main()
 int dataku[5] = {10,20,30,40,50};
 int datamu[5];
 
-cout << "Isi data kedua : " << dataku[1] << endl;
-dataku[1] = 200;
-cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
-
+cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
+    cout << endl;
 }
